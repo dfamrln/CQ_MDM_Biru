c49e545ef2717a806f2a9409900aaea6df397eff
dfamrln <mhdffa1@gmail.com>
2026-03-12 19:42:15 +0700

    Menggunakan ExecuteNon

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -99,5 +99,29 @@ namespace PraktikumADO
                 MessageBox.Show(ex.Message);
             }
         }
+
+        // PRAKTIKUM 4
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Koneksi();
+                conn.Open();
+
+                string query = "UPDATE Mahasiswa SET Alamat='Yogyakarta' WHERE NIM='23110100001'";
+
+                cmd = new SqlCommand(query, conn);
+
+                int hasil = cmd.ExecuteNonQuery();
+
+                MessageBox.Show("Jumlah baris terpengaruh : " + hasil);
+
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
     }
 }
