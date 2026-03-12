58643fa557a0a016fab4de9a6bd692c99dc67335
dfamrln <mhdffa1@gmail.com>
2026-03-12 19:36:16 +0700

    Membuka Koneksi Database

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -33,5 +33,23 @@ namespace PraktikumADO
         {
 
         }
+
+        // PRAKTIKUM 1
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Koneksi();
+                conn.Open();
+
+                MessageBox.Show("Koneksi ke database berhasil");
+
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
     }
 }
